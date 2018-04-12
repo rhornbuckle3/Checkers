ac8877bb35c4e42323a911d7dc6534dd76599e45
Russell Hornbuckle <russspringfield@gmail.com>
2018-04-12 16:07:13 -0400

    Update checkersGame.py

--- checkersGame.py
+++ checkersGame.py
@@ -7,9 +7,35 @@ defaultState=np.array((1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1
 frankOne=None
 frankTwo=None
 activePlayer=None
+stateSequence=np.copy(defaultState)
+stateSequence=stateSequence.reshape((-1,1))
+stateScores=np.array(np.zeros((1,1)))
+#stateSequence=np.append(stateSequence,np.array([0]),axis=0)
+currentState=np.copy(defaultState)
+
+#sequence methods
+def addToSeq(newState,newScore):
+        global stateSequence
+        global stateScores
+        newState=np.array(newState)
+        newScore=np.array(newScore)
+        newScore=newScore.reshape((1,1))
+        #newState=np.append(newState,np.array(newScore))
+        newState=newState.reshape((-1,1))
+        #print(stateScores.shape)
+        #print(newScore.shape)
+        stateSequence=np.append(stateSequence,newState,axis=1)
+        stateScores=np.append(stateScores,newScore,axis=1)
+def getStateSequence():
+    global stateSequence
+    return stateSequence
+
+#initialization
 def initPlayer():
     global activePlayer
     global frankOne
+    #global currentState
+    #global defaultState
     coinFlip=np.random.binomial(0,.5)
     frankOne=cf()
     frankOne.setSide(coinFlip)
@@ -26,10 +52,70 @@ def initPlayer():
         activePlayer=frankTwo
     else:
         activePlayer=frankOne
-def endGame():
-    playOne=frankOne.getStateSequence()
-    playTwo=frankTwo.getStateSequence()
+    #currentState=defaultState
+
+
+
+#Game manager
+def playBall():
+    global stateSequence
+    global currentState
+    global frankOne
+    global frankTwo
+    global activePlayer
+    turn=1
+    while(True):
+        print(turn)
+        print(currentState)
+        newState,newScore=activePlayer.stateDecider(currentState)
+        addToSeq(newState,newScore)
+        check=endGameCheck(newState)
+        if(check):
+            break
+        if(activePlayer==frankOne):
+            activePlayer=frankTwo
+        else:
+            activePlayer=frankOne
+        if(turn>=1200):
+            count=np.sum(newState)
+            if(count>0):
+                print("White Wins")
+                break
+            if(count<0):
+                print("Black Wins")
+                break
+        currentState=newState
+        turn=turn+1
+    endGame()
+
+
+
+#game ending
+def endGameCheck(currentState):
+    global stateSequence
+    #global currentState
+    global frankOne
+    global frankTwo
+    global activePlayer
+    sumBum=0
+    currentState=currentState.reshape((-1,1))
+    for i in range(0,32):
+        sumBum=sumBum+mt.fabs(currentState[i,0])
+    if(mt.fabs(np.sum(currentState))==sumBum):
+        return True
+    return False
+    #playOne=frankOne.getStateSequence()
+    #playTwo=frankTwo.getStateSequence()
     #combine here, or just have each frank hold onto a complete state sequence, or handle state sequence in this one and record each turn
     #combining here would be the cool thing to do
     #recording here would be the elegant thing to do
-    #each frank having a complete state sequence would be the easy thing to do
+def endGame():
+    global stateSequence
+    global stateScores
+    global currentState
+    global frankOne
+    global frankTwo
+    global activePlayer
+    print(activePlayer)
+    print(stateScores)
+    
